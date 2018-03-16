5181fda06092cf37a79d8577cb6d2d867fb76533
Stoner609 <Stoner7969@gmail.com>
2018-03-16 09:48:18 +0800

    update

--- findGetParameter.js
+++ findGetParameter.js
@@ -6,8 +6,8 @@ function findGetParameter(parameterName) {
         .substr(1)
         .split("&")
         .forEach(function (item) {
-          tmp = item.split("=");
-          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+            tmp = item.split("=");
+            if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
         });
     return result;
 }
@@ -16,5 +16,32 @@ function findGetParameter(parameterName) {
 ex: https:index.html?A=123&B=456
 input: findGetParameter('A'); 
 out:   123
-*/ 
+*/
 
+// 取網址列參數
+function parse_query_string(query) {
+    var vars = query.split("&");
+    var query_string = {};
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        // If first entry with this name
+        if (typeof query_string[pair[0]] === "undefined") {
+            query_string[pair[0]] = decodeURIComponent(pair[1]);
+            // If second entry with this name
+        } else if (typeof query_string[pair[0]] === "string") {
+            var arr = [query_string[pair[0]], decodeURIComponent(pair[1])];
+            query_string[pair[0]] = arr;
+            // If third or later entry with this name
+        } else {
+            query_string[pair[0]].push(decodeURIComponent(pair[1]));
+        }
+    }
+    return query_string;
+}
+
+var query = window.location.search.substring(1);
+// Para=A,B,C
+
+var qs = parse_query_string(query);
+// Object 型別
+// { Para: A,B,C }

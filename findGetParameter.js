af1eedc77f3f92e30468b43996b796ccc7b114a6
Stoner609 <Stoner7969@gmail.com>
2018-03-16 09:17:57 +0800

    update

---
+++ findGetParameter.js
@@ -0,0 +1,20 @@
+// 取網址列參數
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+          tmp = item.split("=");
+          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
+}
+
+/*
+ex: https:index.html?A=123&B=456
+input: findGetParameter('A'); 
+out:   123
+*/ 
+
